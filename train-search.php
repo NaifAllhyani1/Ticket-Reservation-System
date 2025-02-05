680dcf4800cb0efb58cac392b25927b5b4c2fa3c
hussam-11 <ahsoooma_999_@hotmail.com>
2025-02-05 13:14:31 +0300

    version 3

--- train-search.php
+++ train-search.php
@@ -6,7 +6,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Train Search</title>
-  <link rel="stylesheet" href="index2.css">
+  <link rel="stylesheet" href="index.css">
 </head>
 <body>
   
